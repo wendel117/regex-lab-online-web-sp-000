2d23dc4c1778b67ad802de91ed0d58690bb5500d
Wendy Hamilton <wdhamilton117@gmail.com>
2019-02-08 15:36:29 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -23,7 +23,7 @@ def first_word_capitalized_and_ends_with_punctuation?(text)
   end
 
 def valid_phone_number?(phone)
-  if phone.match(/\d{10}\s/)
+   if phone.match(/([0-9] *?){10}|(\([0-9]{3}\)(([0-9]{3}-[0-9]{4})|[0-9]{7})\b)/)
     return true
   else
     return false
