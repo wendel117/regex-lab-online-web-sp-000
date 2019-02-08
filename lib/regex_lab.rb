d46ad955533f95235a620d6c101ced276d2e70e3
Wendy Hamilton <wdhamilton117@gmail.com>
2019-02-08 15:22:13 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,5 +1,5 @@
 def starts_with_a_vowel?(word)
-.match(/\b[aeiouAEIOU]/)
+word.match(/\b[aeiouAEIOU]/)
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
@@ -7,7 +7,7 @@ def words_starting_with_un_and_ending_with_ing(text)
 end
 
 def words_five_letters_long(text)
-grep(/\w{5}/)
+text.grep(/\w{5}\s/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
