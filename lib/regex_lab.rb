0441ac2e74c8af3136ef6d119b96ba16d98df32b
Wendy Hamilton <wdhamilton117@gmail.com>
2019-02-08 15:24:07 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,5 +1,8 @@
 def starts_with_a_vowel?(word)
-word.match(/\b[aeiouAEIOU]/)
+if word.match(/\b[aeiouAEIOU]/)
+  return true
+else
+  return false
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
